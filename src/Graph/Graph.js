da3e9b8633a79d6c80373c784a01d2804cf001db
lixiang <348816986@qq.com>
2018-01-31 21:19:50 +0800

    加入事件系统与Graph类

--- src/Graph/Graph.js
+++ src/Graph/Graph.js
@@ -1,14 +1,29 @@
-import EventDispatch from './EventDispatcher';
+import EventDispatcher from './EventDispatcher';
 
-class Graph extends EventDispatch {
-    constructor(x, y, width, height, type) {
-        super();
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.type = type;
-    }
+/**
+ * 图形基类
+ * @param opts
+ * @constructor
+ */
+function Graph(opts) {
+    opts = opts || {};
+
+    EventDispatcher.call(this);
+
+    //shape
+    this.x = opts.x;
+    this.y = opts.y;
+    this.width = opts.width;
+    this.height = opts.height;
+
+    //style
+    this.fill = opts.fill;
+    this.stroke = opts.stroke;
+    this.lineWidth = opts.lineWidth;
 }
+
+Graph.prototype = Object.create(EventDispatcher);
+Graph.prototype.constructor = Graph;
+
 
 export default Graph;
